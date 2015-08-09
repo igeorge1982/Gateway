93691f4e59553c6db4efce97b9ca7c6cf1889ee1
georgegaspar <georgegaspar@052dd0eb-977d-44c6-ba55-d49dfa0f28b1>
2015-08-09 09:05:51 +0000

    rest-master
    git-svn-id: https://localhost/repo/branches/rest-master@216 052dd0eb-977d-44c6-ba55-d49dfa0f28b1

---
+++ BookMyMovie/src/main/java/com/jeet/api/InvalidTicketException.java
@@ -0,0 +1,35 @@
+package com.jeet.api;
+
+public class InvalidTicketException extends BookingException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public InvalidTicketException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidTicketException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidTicketException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidTicketException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidTicketException(String arg0, Throwable arg1, boolean arg2,
+			boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+}
